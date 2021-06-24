0f8ee5d9eaf2e717766805234f23635bac140486
Tin Le <thanhtin.le.vn@gmail.com>
2021-06-24 13:45:45 -0500

    Early protoype of libray form, getting familiar with React

---
+++ frontend/reactform/src/App.js
@@ -0,0 +1,57 @@
+import React, { useReducer, useState } from 'react';
+import './App.css';
+
+const formReducer = (state, event) => {
+ return {
+   ...state,
+   [event.name]: event.value
+ }
+}
+
+function App() {
+  const [formData, setFormData] = useReducer(formReducer, {});
+  const [submitting, setSubmitting] = useState(false);
+
+  const handleSubmit = event => {
+    event.preventDefault();
+    setSubmitting(true);
+
+    setTimeout(() => {
+      setSubmitting(false);
+    }, 3000);
+  }
+
+  const handleChange = event => {
+    setFormData({
+      name: event.target.name,
+      value: event.target.value,
+    });
+  }
+
+  return(
+    <div className="wrapper">
+      <h1>Easy Library</h1>
+      {submitting &&
+        <div>Submtting Form...
+        You are submitting the following:
+                 <ul>
+                   {Object.entries(formData).map(([name, value]) => (
+                     <li key={name}><strong>{name}</strong>:{value.toString()}</li>
+                   ))}
+                 </ul>
+        </div>
+      }
+      <form onSubmit={handleSubmit}>
+        <fieldset>
+          <label>
+            <p>Name</p>
+            <input name="name" onChange={handleChange}/>
+          </label>
+        </fieldset>
+        <button type="submit">Submit</button>
+      </form>
+    </div>
+  )
+}
+
+export default App;
