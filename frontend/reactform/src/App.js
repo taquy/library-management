1c7fb23c5f970ea09b07e2a9a511cfb634cac87e
Tin Le <thanhtin.le.vn@gmail.com>
2021-06-30 15:04:55 -0500

    A better and more complete library submission form

--- frontend/reactform/src/App.js
+++ frontend/reactform/src/App.js
@@ -18,7 +18,7 @@ function App() {
 
     setTimeout(() => {
       setSubmitting(false);
-    }, 3000);
+    }, 3000)
   }
 
   const handleChange = event => {
@@ -30,26 +30,61 @@ function App() {
 
   return(
     <div className="wrapper">
-      <h1>Easy Library</h1>
+      <h1>Book Fields</h1>
       {submitting &&
-        <div>Submtting Form...
-        You are submitting the following:
-                 <ul>
-                   {Object.entries(formData).map(([name, value]) => (
-                     <li key={name}><strong>{name}</strong>:{value.toString()}</li>
-                   ))}
-                 </ul>
+        <div>Submtting Book information...
+          You are submitting the following:
+         <ul>
+           {Object.entries(formData).map(([name, value]) => (
+             <li key={name}><strong>{name} </strong> : {value.toString()}</li>
+           ))}
+         </ul>
         </div>
       }
       <form onSubmit={handleSubmit}>
         <fieldset>
+
           <label>
-            <p>Name</p>
+            <p>Book Title</p>
             <input name="name" onChange={handleChange}/>
           </label>
-        </fieldset>
+
+          <label>
+            <p>Author(s)</p>
+            <input name="authors" onChange={handleChange}/>
+          </label>
+
+          <label>
+            <p>Year Published</p>
+            <input type="month" name="yearpublished" onChange={handleChange}/>
+          </label>
+
+          <label>
+            <p>Publisher(s)</p>
+            <input name="publishers" onChange={handleChange}/>
+          </label>
+
+         <label>
+           <p>Genre</p>
+           <select name="Genre" onChange={handleChange}>
+               <option value="">--Please choose an option--</option>
+               <option value="ficiont">Fiction</option>
+               <option value="nonfiction">Non-fiction</option>
+               <option value="history">History</option>
+               <option value="selfhelp">Self-help</option>
+               <option value="novel">novel</option>
+           </select>
+         </label>
+
+         <label>
+           <p>Count</p>
+           <input type="number" name="count" min="0" onChange={handleChange} step="1"/>
+         </label>
+       </fieldset>
+
         <button type="submit">Submit</button>
       </form>
+
     </div>
   )
 }
