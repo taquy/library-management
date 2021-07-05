511cedce7d03e471057248e564f7b4a558e1506f
fool1280 <ng.huyanh.gd@gmail.com>
2021-07-05 05:50:37 -0500

    add index view

--- backend/library/books/views.py
+++ backend/library/books/views.py
@@ -1,1 +1,15 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.shortcuts import get_object_or_404
+from .models import Book
+
+# Display the all books
+
+
+def index(request):
+    books = Book.objects.all()
+    return render(request, 'books/index.html', {'books': books})
+
+
+def details(request, book_id):
+    return HttpResponse("test" + str(book_id))
